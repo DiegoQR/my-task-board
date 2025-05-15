660f3cce54ea6e6151aacf9aeb076d85c32bb837
DiegoQR <diegoqr2000@gmail.com>
2025-05-15 18:30:46 -0400

    Refactor boardsRouter to implement GET endpoint for retrieving a board by ID

--- backend/src/routes/boardsRouter.ts
+++ backend/src/routes/boardsRouter.ts
@@ -3,9 +3,15 @@ import { Router } from "express-serve-static-core";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    console.log("GET request to /api/boards");
-    res.json({ message: "Hello from boards router!" });
-});
+router.get ("/:id", getBoardBtId);
+/* router.post ("/", createBoard);
+router.put ("/:id", updateBoard);
+router.delete ("/:id", deleteBoard); */
 
 export default (app: { use: (arg0: string, arg1: Router) => any; }) => app.use("/api/boards", router);
+
+async function getBoardBtId(req: express.Request, res: express.Response, next: express.NextFunction) {
+  const { id } = req.params;
+  console.log(id);
+  res.status(200).json({ message: "ok" });
+}
