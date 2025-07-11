4a31bb882cb439472b56136f1cc59b62cf6892a3
DiegoQR <diegoqr2000@gmail.com>
2025-07-10 20:29:20 -0400

    Added rounded border to add new task button

--- packages/frontend/src/components/AddNewTaskButton.tsx
+++ packages/frontend/src/components/AddNewTaskButton.tsx
@@ -1,6 +1,6 @@
 function AddNewTaskButton() {
     return (<>
-        <button className="flex items-center gap-5 bg-soft-yellow px-4 py-5">
+        <button className="flex items-center gap-5 bg-soft-yellow px-4 py-5 rounded-xl">
             <img src="/Add_round_duotone.svg" alt="plus-icon" className="bg-orange p-3 rounded-xl"/>
             <p className="font-bold text-xl">Add new Task</p>
         </button>
